6558b95de56210f3a50fac00107318bdfe717fc2
dolev <dolevizacson@gmail.com>
2021-07-22 20:50:21 +0300

    Auto stash before merge of "master" and "origin/master"
    changes to require statements, change default rout handler to get all

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-// initialization
-const { modules, files, functions, routes } = require('./env/utils/access');
+// environment files
+const routes = require('./env/constants/routes');
 
 // modules
-const express = modules.EXPRESS;
-modules.DOTENV.config();
+const express = require('express');
+const dotenv = require('dotenv');
+
+// environment variables init
+dotenv.config();
 
 // files
-const mongoInit = require(files.MONGO);
-const errorHandlers = require(files.ERROR_HANDLERS);
-const middleware = require(files.MIDDLEWARE);
-const mailerInit = require(files.MAILER_AGENT);
+const mongo = require('./env/DB/mongo/connection-settings');
+const errorHandlers = require('./env/error-handlers/error-handlers');
+const middleware = require('./env/middleware/middleware');
+const mailerAgent = require('./env/mailer-agent/mailer-agent');
 
 // errors
-const BadEndpointError = require(files.BAD_ENDPOINT_ERROR);
+const BadEndpointError = require('./env/errors/bad-endpoint-error');
 
-// db connections
-mongoInit();
+// db connections init
+mongo.mongoInit();
 
 // mailer init
-mailerInit();
+mailerAgent.mailerInit();
 
 // controllers
-const auth = require(files.AUTH);
-const skills = require(files.SKILLS);
-const projects = require(files.PROJECTS);
-const blog = require(files.BLOG);
-const taskList = require(files.TASK_LIST);
-const mailer = require(files.MAILER);
+const auth = require('./auth/controllers/auth-controller');
+const skills = require('./skills/controllers/skills-controller');
+const projects = require('./projects/controllers/projects-controller');
+const blog = require('./blog/controllers/blog-controller');
+const taskList = require('./task-list/controllers/task-list-controller');
+const mailer = require('./mailer/controllers/mailer-controller');
 
 process.on('uncaughtException', function (err) {
-  //mailer logic to report on problems with the app
+  // TODO: mailer logic to report on problems with the app
+  console.err(err);
 });
 
 const app = express();
 
-middleware.addMiddleware(app);
+middleware.addAppMiddleware(app);
 
 // routes
 app.use(routes.AUTH, auth);
@@ -45,10 +49,10 @@ app.use(routes.TASK_LIST, taskList);
 app.use(routes.CONTACT, mailer);
 
 // default route handler
-app.get('*', (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(
     new BadEndpointError(
-      'No such route available',
+      'No such endpoint available',
       req.hostname + req.originalUrl
     )
   );
