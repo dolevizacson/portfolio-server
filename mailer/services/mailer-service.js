646ff0b3274598014c6ba4d8aa07b4114dd6ea31
dolev <dolevizacson@gmail.com>
2022-01-23 12:54:33 +0200

    fix to email message format

--- mailer/services/mailer-service.js
+++ mailer/services/mailer-service.js
@@ -7,10 +7,13 @@ const MailNotSentError = require('../../env/errors/mail-not-sent-error');
 module.exports = class MailerService {
   async sendMail(mail) {
     const mailerTransporter = mailer.getMailerTransporter();
+    const textWithEmail = `Send from: ${mail.from} \n ${mail.text}`;
 
     try {
       const response = await mailerTransporter.sendMail({
         ...mail,
+        text: textWithEmail,
+        replyTo: mail.from,
         to: process.env.CONTACT_EMAIL,
       });
       if (response.rejected.length > 0) {
